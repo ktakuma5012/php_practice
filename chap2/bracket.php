4f1d36fa5df65b36ffa4170ec38228fa69897e84
ktakuma5012 <kishikawa.t28@gmail.com>
2022-01-26 15:12:42 +0900

    Create bracket.php

---
+++ chap2/bracket.php
@@ -0,0 +1,15 @@
+<?php
+
+$data[0] = '鈴木';
+$data[1] = '松井';
+$data[2] = '高橋';
+$data[4] = '村上';
+// 入れ替え
+$data[0] = '筒香';
+// 末尾に追加
+$data[] = '菊池';
+
+var_dump($data);
+//var_dump($data);
+print_r($data);
+?>
