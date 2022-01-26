9b6fe3829905bdee535290781972a25384b4aa19
ktakuma5012 <kishikawa.t28@gmail.com>
2022-01-26 15:22:00 +0900

    Create hello.php

---
+++ chap2/hello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHPの基本構文</title>
+    </head>
+    <body>
+    <?php
+        print 'Hello world!';
+        print 'こんにちは、皆さん';
+    ?>
+    </body>
+</html> 
