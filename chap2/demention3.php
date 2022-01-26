47e8869bb04b5034d5ccef3c97625b57fed04159
ktakuma5012 <kishikawa.t28@gmail.com>
2022-01-26 15:20:27 +0900

    Create demention3.php

---
+++ chap2/demention3.php
@@ -0,0 +1,24 @@
+<?php
+
+$date1 = [
+    [
+        'name' => '山田太郎',
+        'age'  => '35',
+        'sex'  => '男',
+    ],
+    [
+        'name' => '鈴木二郎',
+        'age'  => '30',
+        'sex'  => '男',
+    ],
+    [
+        'name' => '岸川拓磨',
+        'age'  => '28',
+        'sex'  => '男',
+    ]
+];
+
+print_r($date1);
+print_r($date1[0]['name']);
+print_r($date1[2]['name']); 
+?>
