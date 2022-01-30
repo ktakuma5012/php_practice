5063357ede87d0dab73f9289dc5ae7ff9a65d90f
ktakuma5012 <kishikawa.t28@gmail.com>
2022-01-30 15:24:23 +0900

    Create foreach_assoc.php

---
+++ chap4/foreach_assoc.php
@@ -0,0 +1,12 @@
+<?php
+
+$data = [ 'mastui' => '55',
+          'takahashi' => '27',
+          'mitani' => '56',
+          'nomoto' => '23'
+        ];
+
+foreach($data as $key => $value){
+    print "{$key}の年齢は{$value}です<br/>";
+}
+?>
