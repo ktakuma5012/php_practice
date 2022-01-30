1b8379c77f4b56c49623abd797180ac25519cf1a
ktakuma5012 <kishikawa.t28@gmail.com>
2022-01-30 15:29:14 +0900

    Create switch1.php

---
+++ chap4/switch1.php
@@ -0,0 +1,19 @@
+<?php
+$rank = '甲';
+
+
+switch ($rank){
+    case '甲' :
+        print '大変良いです';
+        break;
+    case '乙' :
+        print '良いです';
+        break;
+    case '柄' :
+        print 'もう少しがんばって';
+        break;
+    default :
+        print '???';
+        break;
+}
+?>
