5b8a7cce4a4d25dd70e40195c52d0ef719775a51
ktakuma5012 <kishikawa.t28@gmail.com>
2022-02-16 01:13:23 +0900

    Create array_slice.php

---
+++ chap5/array_slice.php
@@ -0,0 +1,10 @@
+<?php
+// 特定の要素を取得してくる。
+$data = ['kishikawa','takahashi','suzuki'];
+
+print_r(array_slice($data, 2, 3));
+// keyも維持できる。
+print_r(array_slice($data, 2, 3, true));
+// 0から末尾まで取得できる。
+print_r(array_slice($data, 0));
+?>
