af9f78ed283a446addfb7b52822811af74ed11f9
ktakuma5012 <kishikawa.t28@gmail.com>
2022-02-16 00:27:56 +0900

    Create strtolower.php

---
+++ chap5/strtolower.php
@@ -0,0 +1,18 @@
+<?php
+
+//すべて小文字
+print strtolower('Kishikawa Php Blog');
+echo PHP_EOL;
+//すべてお文字
+print strtoupper('kishikawa php blog');
+echo PHP_EOL;
+
+print lcfirst('Kishikawa php Blog');
+echo PHP_EOL;
+print ucfirst('kishikawa php blog');
+echo PHP_EOL;
+//最初の始まりの文字は大文字にする。
+print ucwords('kishikawa php blog');
+echo PHP_EOL;
+
+?>
